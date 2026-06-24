19ed93faf863921d6a8c4e6696f962840418e450
Konstantinos Pl. <161327003+KonstantinosPl@users.noreply.github.com>
2026-06-24 21:17:15 +0300

    Expose public API

--- hydrotrends/__init__.py
+++ hydrotrends/__init__.py
@@ -3,3 +3,17 @@ HydroTrends: hydroclimatic trend analysis tools.
 """
 
 __version__ = "0.1.0"
+
+from hydrotrends.analysis import (
+    estimate_mean_per_polygon_from_monthly_file,
+    mann_kendall_test,
+    compute_daily_stats_by_polygon,
+)
+
+from hydrotrends.extreme_climate_indices.temperature import (
+    temperature_extreme_indices,
+)
+
+from hydrotrends.extreme_climate_indices.precipitation import (
+    precipitation_extreme_indices,
+)
