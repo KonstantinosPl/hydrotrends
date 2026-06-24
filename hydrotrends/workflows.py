8bdf5caedeabb45a77dd7b867a51c05589600075
Konstantinos Pl. <161327003+KonstantinosPl@users.noreply.github.com>
2026-06-24 22:20:51 +0300

    Refactor aggregation API

---
+++ hydrotrends/workflows.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+from hydrotrends.analysis import (aggregate_monthly, mann_kendall_test)
+
+def run_trend_workflow(
+    variable_paths,
+    shapefiles,
+    output_dir,
+    modes=("monthly", "seasonal", "annual"),
+):
+    output_dir = Path(output_dir)
+
+    for input_file in variable_paths:
+
+        for region_name, shapefile_path  in shapefiles.items():
+            
+            region_output_dir = output_dir / region_name
+
+            var_name = aggregate_monthly(
+                input_file=input_file,
+                shapefile_path=shapefile_path,
+                output_folder=region_output_dir,
+            )
+
+            for mode in modes:
+
+                 for mode in modes:
+                    mk_input_file = (
+                        region_output_dir
+                        / var_name
+                        / mode
+                        / f"{mode}_{var_name}.xlsx"
+                    )
+
+                    mann_kendall_test(
+                        input_file=mk_input_file,
+                        shapefile_path=shapefile_path,
+                        output_folder=region_output_dir,
+                        var_name=var_name,
+                        mode=mode,
+                    )
+
+    return
